a1d7951c7364eae66dde765d5edec57526e8638b
Komborerai Chikweshe <kombogc@gmail.com>
2018-05-09 21:15:19 +0200

    Second Commit of EMS Web Application

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 # third-party imports
 import flask_sqlalchemy
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 from config import app_config
@@ -14,42 +13,48 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    db.init_app(app)
-    Bootstrap(app)
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
 
-    from app import models
+from app import models
 
-    from app import models
+from app import models
 
-    from .admin import admin as admin_blueprint
+from .admin import admin as admin_blueprint
 
-    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
-    from .auth import auth as auth_blueprint
+from .auth import auth as auth_blueprint
 
-    app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint)
 
-    from .home import home as home_blueprint
+from .home import home as home_blueprint
 
-    app.register_blueprint(home_blueprint)
+app.register_blueprint(home_blueprint)
 
-    @app.errorhandler(403)
-    def forbidden(error):
-        return render_template('errors/403.html', title='Forbidden'), 403
 
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template('errors/404.html', title='Page Not Found'), 404
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html', title='Forbidden'), 403
 
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return render_template('errors/500.html', title='Server Error'), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html', title='Page Not Found'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html', title='Server Error'), 500
+
 
     return app
